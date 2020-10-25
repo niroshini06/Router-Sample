1ead48ec320ccdb834498d8c4d9a9fc03db2cdbe
niroshini <niroshini06@gmail.com>
2020-10-25 14:13:46 +0530

    route info passed via props

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const Contacts = () => {
+const Contacts = (props) => {
+	console.log(props);
+	setTimeout(() => {
+		props.history.push('/about');
+	}, 2000);
 	return (
 		<div className="container">
 			<h4 className="center"> Contacts</h4>

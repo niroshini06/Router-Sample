e0dddf3a330a3cac366b38ecc6b1c1ac451420e9
niroshini <niroshini06@gmail.com>
2020-10-27 16:10:10 +0530

    axios

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Home = () => {
-	return (
-		<div className="container">
-			<h4 className="center"> Home </h4>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy
-				text ever since the 1500s, when an unknown printer took a galley
-				of type and scrambled it to make a type specimen book. It has
-				survived not only five centuries, but also the leap into
-				electronic typesetting, remaining essentially unchanged.
-			</p>
-		</div>
-	);
-};
+class Home extends Component {
+	state = {
+		posts: [],
+	};
+	componentDidMount() {
+		axios.get('https://jsonplaceholder.typicode.com/todos/').then((res) => {
+			this.setState({
+				posts: res.data.slice(0, 10),
+			});
+		});
+	}
+
+	render() {
+		const posts = this.state.posts;
+		const postsList = posts.length ? (
+			posts.map((e) => {
+				return (
+					<div className="post card" key={e.id}>
+						<div className="card-content">
+							<span className="card-title">{e.title}</span>
+						</div>
+					</div>
+				);
+			})
+		) : (
+			<div className="center"> No posts available</div>
+		);
+		return (
+			<div className="container">
+				<h4 className="center"> Home </h4>
+				{postsList}
+			</div>
+		);
+	}
+}
 
 export default Home;

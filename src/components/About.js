91635a8092ea37177b4333fec0d53c3d36062271
niroshini <niroshini06@gmail.com>
2020-10-27 12:50:47 +0530

    higher order component - part 2

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Rainbow from '../hoc/Rainbow';
 
 const About = () => {
 	return (
@@ -16,4 +17,4 @@ const About = () => {
 	);
 };
 
-export default About;
+export default Rainbow(About);
